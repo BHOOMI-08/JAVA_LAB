fadb763d70a130fbcd62adcf418941958ad1d797
BHOOMI ARORA <bhoomiarora.0808@gmail.com>
2026-01-31 00:31:38 +0530

    Add StudentInfo class to display student details

---
+++ LAB_1.java
@@ -0,0 +1,12 @@
+public class StudentInfo {
+    public static void main(String[] args) {
+        String name = "Bhoomi Arora";
+        String sapId = "590015099";
+        String school = "School of Computer Science";
+        String program = "B.Tech CSE";
+        System.out.println("Name: " + name);
+        System.out.println("SAP ID: " + sapId);
+        System.out.println("School: " + school);
+        System.out.println("Program: " + program);
+    }
+}
